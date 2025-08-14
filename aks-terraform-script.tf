4633de7de3ac7cb7817edde18740cf4966afe8e5
NithishReddyGithub <nithishreddyp9666@gmail.com>
2025-08-14 12:14:10 +0530

    modified provider

--- aks-terraform-script.tf
+++ aks-terraform-script.tf
@@ -12,11 +12,6 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
   location = var.location
